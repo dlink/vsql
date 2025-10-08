32944a74cbb4e04c37b49f4e0caec153ac830628
David Link <dvlink@gmail.com>
2025-10-08 09:15:48 -0400

    Format create: adding blank lines where needed

--- format_create_table.py
+++ format_create_table.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import re
 import sys
 
@@ -16,61 +17,39 @@ def format_create(sql: str) -> str:
         if line.startswith('table'):
             continue
         parts = line.split(' ')
+        # remove tablename column
+        if parts[1] == 'create' and parts[2] == 'table':
+            parts = parts[1:]
+        # handle leading spaces
         if parts[0] == '' and parts[1] == '':
             fieldname = parts[2]
             fieldnames.append(fieldnames)
             max_fieldname_len = max(max_fieldname_len, len(fieldname))
         lines.append(parts)
-    #lines = []
-    #fieldnames = []
-    #max_fieldname_len = 0
+
+    # create output
+    key_found = 0
+    constraint_found = 0
     lines2 = []
     for parts in lines:
         if parts[0] == '' and parts[1] == '':
+            # add blank line before keys
+            if parts[2] == 'key' and not key_found:
+                lines2.append('')
+                key_found = 1
+            # add blank line before constraints
+            if parts[2] == 'constraint' and not constraint_found:
+                lines2.append('')
+                constraint_found = 1
             parts[2] = parts[2].ljust(max_fieldname_len)
+        # add blank line before closing ')'
+        if parts[0] == ')':
+            lines2.append('')
         lines2.append(' '.join(parts))
-            
+    # add semicolon at the end
+    lines2.append(';')
+
     return '\n'.join(lines2)
-
-    #print(max_fieldname_len)
-    #return "\n".join(lines)
-
-    #return sql
-    # Normalize case of main keywords
-    # keywords = [
-    #     "create table", "primary key", "unique key", "key",
-    #     "constraint", "foreign key", "references",
-    #     "engine=", "default charset="
-    # ]
-    # for kw in keywords:
-    #     sql = re.sub(kw, kw.lower(), sql, flags=re.I)
-
-    # Split commas into newlines (but not inside parentheses like decimal(12,4))
-    #sql = re.sub(r",\s+([A-Za-z])", ",\n\\1", sql)
-
-    # lines = []
-    # inside = False
-    # for line in sql.splitlines():
-    #     line = line.rstrip()
-    #     # if line.lower().startswith("create table"):
-    #     #     lines.append(line)
-    #     #     inside = True
-    #     #     continue
-    #     # if inside:
-    #     #     if line.startswith(")"):
-    #     #         inside = False
-    #     #         lines.append(")")
-    #     #         continue
-    #     #     if line:
-    #     #         lines.append("   " + line)  # indent 3 spaces
-    #     # else:
-    #     #     lines.append(line)
-
-    # # Ensure closing semicolon
-    # if not lines[-1].strip().endswith(";"):
-    #     lines[-1] = lines[-1] + "\n;"
-
-    #return "\n".join(lines)
 
 if __name__ == "__main__":
     raw = sys.stdin.read()
